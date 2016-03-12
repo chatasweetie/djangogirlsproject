1169f14e6104a9bb7e89cc6f90477a4e4c264913
Jessica <jessica.dene.earley@gmail.com>
2016-03-12 08:35:22 -0800

    sourced my keys, this project is not for depolyment

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@@gjqsb4l0am$zjp(uax0!_p1u)ox26l+ll8kpcb-^_%lcq^6n'
+# Source your secret key
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
